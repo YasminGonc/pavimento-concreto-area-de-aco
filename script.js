fa2299aeddbc7fe6cf023b70eaec29efd657b26a
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-07-04 22:06:02 -0300

    ajustes JS

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ function calcular(){
         diamVerif.innerHTML = 'Digite um valor válido!';
     }
 
-    var areaTelaComp = (25459 * 0.0868 * (espPlaca * 0.0254) * (compPlaca * 0.0254) * 1.5) / (aco*10*145*2);
-    var areaTelaLarg = (25459 * 0.0868 * (espPlaca * 0.0254) * (largPlaca * 0.0254) * 1.5) / (aco*10*145*2);
+    var areaTelaComp = (25459 * 0.0868 * (espPlaca * 39.3701) * (compPlaca * 39.3701) * 1.5) / (aco*10*145*2);
+    var areaTelaLarg = (25459 * 0.0868 * (espPlaca * 39.3701) * (largPlaca * 39.3701) * 1.5) / (aco*10*145*2);
 
     /*Vai apresentar na tela o maior resultado entre as áreas*/
     if( areaTelaComp > areaTelaLarg){
@@ -39,20 +39,22 @@ function calcular(){
         document.querySelector("#resTela").innerHTML = areaTelaLarg.toFixed(2);
     }
    
-    var areaBar = (25459 * 0.0868 * (espPlaca * 0.0254) * (largPlaca * 0.5 * 0.0254) * 1.5) / 72500;
-    var espaBar =  (Math.PI * (diamBarra ** 2) / 4) / areaBar;
+    var areaBar = (25459 * 0.0868 * (espPlaca * 39.3701) * (largPlaca * 0.5 * 39.3701) * 1.5) / 72500;
+    var diam = (Math.PI * ((diamBarra/10) * (diamBarra/10))) / 4;
+    var espaBar = diam / (areaBar / 100);
     var compBar = ((0.5 * 72500 * (diamBarra / 25.4) / 350) + 3) * 0.0254;
+
     
     document.querySelector("#esp-bar-lig").innerHTML = espaBar.toFixed(2);
     document.querySelector("#comp-bar-lig").innerHTML = compBar.toFixed(2);
     
 }
 function limpar(){
-    document.querySelector("#acoVerif").innerHTML = " ";
-    document.querySelector("#compVerif").innerHTML = " ";
-    document.querySelector("#largVerif").innerHTML = " ";
-    document.querySelector("#espVerif").innerHTML = " ";
-    document.querySelector("#diamVerif").innerHTML = " ";
+    document.querySelector("#aco").value = " ";
+    document.querySelector("#comp-placa").value = " ";
+    document.querySelector("#larg-placa").value = " ";
+    document.querySelector("#esp-placa").value = " ";
+    document.querySelector("#diam-barra").value = " ";
 
     document.querySelector("#resTela").innerHTML = "0,0";
     document.querySelector("#esp-bar-lig").innerHTML = "0,0";
